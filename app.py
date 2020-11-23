27513e0aa6067e438405c4621e69f98b11f73d0d
shreyas.arjun007@gmail.com <shreyas.arjun007@gmail.com>
2020-11-23 22:48:38 +0530

    app.py file updated

--- app.py
+++ app.py
@@ -2,9 +2,16 @@
 from flask import Flask, render_template, request
 import pickle
 
+#pre processing models
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import re
+
 # Load the Multinomial Naive Bayes model and CountVectorizer object from disk
 cv = pickle.load(open('cv-transform.pkl','rb'))
 classifier = pickle.load(open('restaurant-sentiment-mnb-model.pkl', 'rb'))
+
 
 
 app = Flask(__name__)
@@ -16,8 +23,13 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
+    	ps=PorterStemmer()
     	message = request.form['message']
-    	data = [message]
+    	message = re.sub('[^a-zA-Z]',' ',message).lower().split()
+    	review_words = [word for word in message if not word in set(stopwords.words('english'))-{'not'}]
+    	review = [ps.stem(word) for word in review_words]
+    	review = ' '.join(review)
+    	data=[review]
     	vect = cv.transform(data).toarray()
     	my_prediction = classifier.predict(vect)
     	return render_template('result.html', prediction=my_prediction)
